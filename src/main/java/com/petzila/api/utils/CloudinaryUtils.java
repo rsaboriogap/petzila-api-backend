c07537822b57c89cddab080d7600ad6a735cf244
vicente <vsansilvestregap@wearegap.com>
2015-01-16 10:12:48 -0600

    adding error properties

---
+++ src/main/java/com/petzila/api/utils/CloudinaryUtils.java
@@ -0,0 +1,31 @@
+package com.petzila.api.utils;
+
+import com.cloudinary.Cloudinary;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Created by vicente on 16/01/15.
+ */
+public class CloudinaryUtils {
+
+    private Cloudinary cloudinary = new Cloudinary(Cloudinary.asMap(
+            "cloud_name", "123",//NOT UPLOAD VALID CREDENTIALS AS IS A PUBLIC GITHUB REPOSITORY
+            "api_key", "123",
+            "api_secret", "123"));
+
+
+    public Map uploadImage(byte[] imageData) throws IOException {
+        return cloudinary.uploader().upload(imageData, Cloudinary.emptyMap());
+    }
+
+    public Map uploadImage(String imageData) throws IOException {
+        try{
+            return cloudinary.uploader().upload(imageData, Cloudinary.emptyMap());
+        }catch(Exception ex){
+            System.out.println(ex.toString());
+        }
+        return null;
+    }
+}
